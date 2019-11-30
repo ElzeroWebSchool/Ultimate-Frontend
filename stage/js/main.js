b3679615ab2081e509a1e487ae58396462f68e2c
ElzeroWebSchool <zeroic.soul@gmail.com>
2019-12-01 00:49:11 +0300

    Lesson 18

--- stage/js/main.js
+++ stage/js/main.js
@@ -12,4 +12,47 @@ $(function() {
       .next(".child-links")
       .slideToggle();
   });
+  // Open / Close Fullscreen
+  $(".toggle-fullscreen").on("click", function() {
+    $(this).toggleClass("full-screen");
+    if ($(this).hasClass("full-screen")) {
+      // Page Is Now Full Screen
+      openFullscreen();
+    } else {
+      // Page is Not Full Screen
+      closeFullscreen();
+    }
+  });
 });
+
+var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
